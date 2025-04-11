b39cd17e84cee953e5a05ae03a073612a8dec4ac
Ykitsoukou <yollankitsoukou@gmail.com>
2025-04-11 11:36:52 -0400

    updated variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,8 @@
-# variables.tf
 variable "eks_auth_users" {
-  description = "List of IAM users to add to aws-auth configmap"
   type = list(object({
     userarn  = string
     username = string
     groups   = list(string)
   }))
+  sensitive = true
 }
